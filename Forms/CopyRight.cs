6fe2a927d91e20d17162326c2480cd21aec0556e
coman <coman@VICTUS15>
2023-12-26 14:18:07 +0100

    Agregar archivos de proyecto.

---
+++ Forms/CopyRight.cs
@@ -0,0 +1,10 @@
+namespace Dango_Futbol_6._0
+{
+    public partial class CopyRight : Form
+    {
+        public CopyRight()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+namespace Dango_Futbol_6._0
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new CopyRight());
+        }
+    }
+}
